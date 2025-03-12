ff661da427f05b7a5705feab3bb99632b7b95e40
VictoriaDaneva <viktoriadanevas2003s@gmail.com>
2025-03-12 15:21:21 +0200

    added edit pet and profile pages + css
    for now i will not put them in the router (later i need them to fetch data)

---
+++ front-end-part/src/components/Edit-Pet/EditPet.jsx
@@ -0,0 +1,69 @@
+export default function editPet() {
+  return (
+    <div class="card">
+      <h2>Edit a Pet Information</h2>
+      <form action="/update-product" method="post" class="form">
+        <input
+          type="url"
+          id="image-url"
+          name="imageUrl"
+          placeholder="Image URL"
+          required
+        />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Pet Name"
+          required
+        />
+
+        <input
+          type="text"
+          id="bred"
+          name="breed"
+          placeholder="Pet Breed"
+          required
+        />
+        <input
+          type="text"
+          id="type"
+          name="author"
+          placeholder="Author"
+          required
+        />
+        <select
+          id="pet-type"
+          name="pet-type"
+          placeholder="Select Pet Type"
+          required
+        >
+          <option value="" disabled selected>
+            Select Pet Type
+          </option>
+          <option value="dog">Dog</option>
+          <option value="cat">Cat</option>
+          <option value="bird">Bird</option>
+          <option value="rabbit">Rabbit</option>
+        </select>
+        <textarea
+          id="description"
+          name="description"
+          placeholder="Description"
+          rows="3"
+          required
+        ></textarea>
+        <div class="button-group">
+          <button type="submit">Save Changes</button>
+          <button
+            type="button"
+            class="cancel-btn"
+            onclick="window.location.href='/products'"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ front-end-part/src/components/Edit-Profile/EditProfile.jsx
@@ -0,0 +1,46 @@
+export default function EditProfile() {
+  return (
+    <div class="card">
+      <h2>Edit Profile</h2>
+      <form action="" method="post" class="form">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value="User Name"
+          required
+        />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value="mail@expaytech.co"
+          required
+        />
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          placeholder="Phone Number"
+          value="+20 01274318900"
+          required
+        />
+        <textarea
+          id="address"
+          name="address"
+          placeholder="Address"
+          rows="3"
+          required
+        >
+          285 N Broad St, Elizabeth, NJ, 07208, USA
+        </textarea>
+        <button type="submit">Save Changes</button>
+        <button type="button" class="cancel-btn">
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+}
